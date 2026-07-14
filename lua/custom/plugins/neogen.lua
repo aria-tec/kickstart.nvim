e6d142599bbb6ca55aeee1d5c41696cc7acae10d
arias <aria-tec@gmail.com>
2026-07-15 00:12:33 +0700

    feat: add modular neogen.lua config for annotation generation

---
+++ lua/custom/plugins/neogen.lua
@@ -0,0 +1,19 @@
+-- neogen.lua
+-- https://github.com/danymat/neogen
+
+-- Pemasangan plugin neogen
+vim.pack.add { 'https://github.com/danymat/neogen' }
+
+require('neogen').setup {
+  enabled = true,
+  input_after_comment = true, -- Masuk ke mode insert setelah anotasi disisipkan
+  snippet_engine = 'nvim', -- Menggunakan engine snippet bawaan Neovim 0.10+ / 0.11+
+}
+
+-- [[ Keymaps ]]
+local map = vim.keymap.set
+
+map('n', '<leader>dG', '<cmd>Neogen<CR>', { desc = '[D]oc [G]enerate (Auto)' })
+map('n', '<leader>df', '<cmd>Neogen func<CR>', { desc = '[D]oc [F]unction' })
+map('n', '<leader>dc', '<cmd>Neogen class<CR>', { desc = '[D]oc [C]lass' })
+map('n', '<leader>dt', '<cmd>Neogen type<CR>', { desc = '[D]oc [T]ype' })
